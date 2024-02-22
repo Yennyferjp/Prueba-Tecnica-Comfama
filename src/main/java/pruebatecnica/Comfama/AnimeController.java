e73c67ddaa1ea5ec867c7ff353bdde11202f3e29
Yennyferjp <yennyfer.jarava92@gmail.com>
2024-02-22 17:13:47 -0500

    Construcción de un servidor que actúa como intermediario entre los clientes y la API externa de Jikan

--- src/main/java/pruebatecnica/Comfama/AnimeController.java
+++ src/main/java/pruebatecnica/Comfama/AnimeController.java
@@ -3,10 +3,14 @@ package pruebatecnica.Comfama;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 @RestController()
 @RequestMapping(path = "/anime")
@@ -20,10 +24,21 @@ public class AnimeController {
 
     @GetMapping()
     public Page<Anime> getAnime(
-            @RequestParam(value = "title") String title,
+            @RequestParam(value = "title", required = false) String title,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size
             ){
-        return animeService.getAnime(title, PageRequest.of(page, size));
+        Pageable pageable = PageRequest.of(page, size);
+        try {
+            return animeService.getAnime(title, pageable);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
 }
